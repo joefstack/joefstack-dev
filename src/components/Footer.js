bde4c183eeb3534d3b66229134d41207ba1845f0
Joseph Chow <joefstack@gmail.com>
2019-10-26 17:08:57 -0400

    removed tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <div className="copyright">
-      &copy; Joefstack. All rights reserved. Design:
-      <a href="https://html5up.net">HTML5 UP</a>.
+      &copy; Joefstack. All rights reserved.
     </div>
   );
 }
